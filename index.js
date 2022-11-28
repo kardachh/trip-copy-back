2ec50f013ada905eb7c2039d62a0e83c85468155
kardachh <kardakov.denis49@gmail.com>
2022-11-28 17:54:52 +0300

    fix?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { ref, get, getDatabase } from "firebase/database";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
