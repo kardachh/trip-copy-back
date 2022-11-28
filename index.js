ce7ad36ba9902eed1a19d11ecd4dc1ee12a98da0
kardachh <kardakov.denis49@gmail.com>
2022-11-28 17:01:19 +0300

    fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render('123'))
+  .get('/', (req, res) => res.send('123'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
