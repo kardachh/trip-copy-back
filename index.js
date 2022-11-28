a612643746388d95afbef7edd3d8205e2ec2754e
kardachh <kardakov.denis49@gmail.com>
2022-11-28 16:47:21 +0300

    213

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(123)
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.render('123'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
