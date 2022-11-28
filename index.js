7a4b0041b9f00ad48c0f2017ef87f82809aa7641
kardachh <kardakov.denis49@gmail.com>
2022-11-28 18:22:27 +0300

    test

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Access-Control-Request-Headers"
   );
   console.log("headers added")
   next();
